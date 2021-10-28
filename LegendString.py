d40b71dad74465c63838e91fbfb3c7a1719d55fc
Abexo-koss <93327057+Abexo-koss@users.noreply.github.com>
2021-10-28 19:48:29 +0300

    Update LegendString.py

--- LegendString.py
+++ LegendString.py
@@ -5,16 +5,17 @@ from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
 
-okvai = input("Enter 69 to continue: ")
-if okvai == "69":
+okvai = input("Enter junta to continue: ")
+if okvai == "junta":
     print("Please go to my.telegram.org and get your API Id and API Hash to proceed.")
     APP_ID = int(input("Enter APP ID here: "))
     API_HASH = input("Enter API HASH here: ")
 
     with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
         print(client.session.save())
+        print("check your save message ")
         client.send_message("me", client.session.save())
-        client.send_message("me", "Above is your #JUNTA STRING SESSION ,,, kelay yalew yerso #JUNTA USERBOT STRING SESSION  new \nPaste this string in Heroku Var.\n\n[Team LegendBot](t.me/Official_LegendBot)")
+        client.send_message("me", "Above is your #JUNTA STRING SESSION ,,, kelay yalew yerso #JUNTA USERBOT STRING SESSION  new yehnen  session lemanm endaysetu \nPaste this string in Heroku Var.\n\n[Developer Abex kos](t.me/JUNTAUSERBOT")
 
 else:
     print("yetesasate neger asgebtewal")
